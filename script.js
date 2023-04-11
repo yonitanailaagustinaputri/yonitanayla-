aa20b5a09c9e8ca58b473ce7dc7bf7f481d933ba
yonitanailaagustinaputri <124542668+yonitanailaagustinaputri@users.noreply.github.com>
2023-04-11 09:50:58 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// toggle class active
+const navbarNav = document.querySelector('.navbar-nav');
+// ketika hamburger menu di klik
+document.querySelector('#hamburger-menu').
+onclick = () => {
+    navbarNav.classList.toggle('active');
+};
+
+//untuk hamburger
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function(e){
+    if(!hamburger.contains(e.target)&& !navbarNav.contains(e.target)){
+        navbarNav.classList.remove('active');
+    }
+})
